22ac19f43c21ac596ec98c19d4a1b3c4a287cf25
Benjamin Standfield <benjaminstandfield@gmail.com>
2025-08-27 20:54:22 -0400

    initial commit

---
+++ Password.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿namespace Password.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldReturnIneligableForNullString()
+    {
+        Assert.Equal("INELIGABLE", Password.CheckPassword(""));
+    }
+
+    [Fact]
+    public void ShouldReturnWeakForOneCase()
+    {
+        Assert.Equal("WEAK", Password.CheckPassword("a"));
+    }
+
+    [Fact]
+    public void ShouldReturnMediumForTwoCases()
+    {
+        Assert.Equal("MEDIUM", Password.CheckPassword("aA"));
+    }
+
+    [Fact]
+    public void ShouldReturnMediumForThreeCases()
+    {
+        Assert.Equal("MEDIUM", Password.CheckPassword("aA1"));
+    }
+
+    [Fact]
+    public void ShouldReturnStrongForFourCases()
+    {
+        Assert.Equal("STRONG", Password.CheckPassword("@aA1"));
+    }
+}
---
+++ Password/Password.cs
@@ -0,0 +1,46 @@
+﻿namespace Password;
+
+public class Password
+{
+
+    static char[] symbols = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')' };
+
+    public static string CheckPassword(string x)
+    {
+        int count = 0;
+
+        if (x.Any(char.IsUpper))
+        {
+            count++;
+        }
+
+        if (x.Any(char.IsLower))
+        {
+            count++;
+        }
+
+        if (x.Any(char.IsNumber))
+        {
+            count++;
+        }
+
+        if (x.Any(c => symbols.Contains(c)))
+        {
+            count++;
+        }
+
+        switch (count)
+        {
+            case 0:
+                return "INELIGABLE";
+            case 1:
+                return "WEAK";
+            case 2 or 3:
+                return "MEDIUM";
+            case 4:
+                return "STRONG";
+            default:
+                return "INELIGABLE";
+        }
+    }
+}
